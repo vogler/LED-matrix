20117491a330d14177bb64b50c34e4925d686617
Ralf Vogler <ralf.vogler@gmail.com>
2023-02-16 00:10:30 +0100

    wled.py: colors for digits RGBY -> RYGB

--- wled.py
+++ wled.py
@@ -122,9 +122,9 @@ digits[9] = [[1,1,1],
 colors = {
     "black":  [0, 0, 0],
     "red":    [255, 0, 0],
+    "yellow": [255, 255, 0],
     "green":  [0, 255, 0],
     "blue":   [0, 0, 255],
-    "yellow": [255, 255, 0],
 }
 
 # place RGB pixels p at position x, y
